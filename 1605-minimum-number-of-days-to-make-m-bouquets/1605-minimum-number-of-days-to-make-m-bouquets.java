0b1829180b487f5a0b5b10a0e66ce1c54137b2e9
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-05-29 08:56:25 +0530

    Time: 34 ms (5.01%), Space: 56.6 MB (76.93%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,52 @@
 class Solution {
-    boolean possible(int arr[],int min,int k,int m)
-    {
+    public boolean possible(int arr[],int days,int m ,int k)
+    {   
+        System.out.println(days);
+
         int cnt=0;
-        int days=0;
+        int poss=0;
         for(int i=0;i<arr.length;i++)
         {
-            if(arr[i]<=min)
+            if(arr[i]<=days)
             {
                 cnt++;
             }else{
-                days+=(cnt/k);
-                if(m<=days)
-                {
-                    return true;
-                }
+                poss+=(cnt/k);
                 cnt=0;
             }
         }
-        days+=(cnt/k);
-
-        return m<=days;
+        poss+=(cnt/k);
+        System.out.println("bloom : "+ poss);
+        return poss>=m;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        if(k*m > bloomDay.length )
-        {
-            return -1;
-        }
+       if(k*m > bloomDay.length)
+       {
+          return -1;
+       }
+       
+       int min=bloomDay[0];
+       int max=min;
+       for(int i=0;i<bloomDay.length;i++)
+       {
+        min=Math.min(min,bloomDay[i]);
+        max=Math.max(max,bloomDay[i]);
+       }
 
-        int min=bloomDay[0];
-        int max=min;
-
-        for(int i:bloomDay)
+       int days=-1;
+       int l=min,r=max;
+       while(l<=r)
+       {
+        int mid=l+(r-l)/2;
+        if(possible(bloomDay,mid,m,k))
         {
-            min=Math.min(min,i);
-            max=Math.max(max,i);
+            days=mid;
+            r=mid-1;
+        }else{
+            l=mid+1;
         }
-        int l=min,r=max,ans=-1;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(possible(bloomDay,mid,k,m))
-            {
-                ans=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
-        return ans;
+       }
+    
+       return days;
     }
 }
