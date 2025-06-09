34503b24cb31f6978de098c04d4718ba88ed8f2a
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-09 21:16:21 +0530

    Time: 1 ms (99.85%), Space: 53.1 MB (49.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,17 @@
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int cad=nums[0];
         int count=1;
-
+        int cad=nums[0];
         for(int i=1;i<nums.length;i++)
         {
-           if(count==0)
-           {
-            cad=nums[i];
-            count=1;
-           }else{
-            count+= (cad==nums[i]?1:-1);
-           }
+            if(count==0)
+            {
+                cad=nums[i];
+                count=1;
+            }
+            else
+            count+=(cad==nums[i])?1:-1;
         }
         return cad;
     }
