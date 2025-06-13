594bb55b9c0513dbdb2c9d0cf1d7ccbe60d94e15
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-13 09:57:12 +0530

    Time: 55 ms (5.41%), Space: 56.2 MB (83.24%) - LeetHub

--- 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.java
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.java
@@ -1,19 +1,24 @@
 class Solution {
    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
-        int[] result=new int[k];
-        HashMap<Integer,HashSet<Integer>> map=new HashMap<>();
-        
-        for(int[] log:logs){
-            int id=log[0];
-            int t=log[1];
-            if(!map.containsKey(id)) map.put(id,new HashSet<>());
-            map.get(id).add(t);
+       int res[]=new int[k];
+       HashMap<Integer,HashSet<Integer>> map= new HashMap<>();
+
+       for(int []log:logs)
+       {
+        int id=log[0];
+        int action=log[1];
+        if(!map.containsKey(id))
+        {
+            map.put(id,new HashSet<>());
         }
-        
-        for(int id:map.keySet()){
-             int UAM=map.get(id).size();
-             result[UAM-1]++;  
-        }
-        return result;
+        map.get(id).add(action);
+       }
+
+       for(int i:map.keySet())
+       {
+        int uam=map.get(i).size();
+        res[uam-1]++;
+       }
+       return res;
     }
 }
