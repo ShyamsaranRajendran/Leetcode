9dd0bf2fe6ae1e47ce199bd632a3095de2122c8b
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-15 20:06:41 +0530

    Time: 3 ms (94.39%), Space: 45 MB (62.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,30 @@
 class Solution {
-    int func(int i, int j, String s, String t, int[][] dp) {
-        if (i == 0) return j; // Insert all remaining of t
-        if (j == 0) return i; // Delete all remaining of s
+    int func(int i,int j,String s,String t,int dp[][])
+    {
+        if(i==0) 
+        return j;
+        if(j==0)
+        return i;
+        if(dp[i][j]!=-1)
+          return dp[i][j];
+        if(s.charAt(i-1)==t.charAt(j-1))
+        {
+            return dp[i][j]= 0 + func(i-1,j-1,s,t,dp);
+        }else{
+            int d= 1 + func(i-1,j,s,t,dp);
+            int ins= 1+ func(i,j-1,s,t,dp);
+            int  rep = 1+ func(i-1,j-1,s,t,dp);
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s.charAt(i - 1) == t.charAt(j - 1)) {
-            return dp[i][j] = func(i - 1, j - 1, s, t, dp);
-        } else {
-            int del = 1 + func(i - 1, j, s, t, dp);
-            int ins = 1 + func(i, j - 1, s, t, dp);
-            int rep = 1 + func(i - 1, j - 1, s, t, dp);
-            return dp[i][j] = Math.min(del, Math.min(ins, rep));
+            return dp[i][j]= Math.min(d,Math.min(ins,rep));
         }
     }
-
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return func(n, m, word1, word2, dp);
+        int n=word1.length();
+        int m=word2.length();
+        int dp[][]=new int [n+1][m+1];
+        for(int i=0;i<=n;i++)
+          Arrays.fill(dp[i],-1);
+        int res=func(n,m,word1,word2,dp);
+        return res;
     }
 }
