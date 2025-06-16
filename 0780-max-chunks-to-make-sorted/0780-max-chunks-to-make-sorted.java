8984dee1a770d20ef81310dc1e4cfaaad830d400
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-16 14:29:58 +0530

    Time: 0 ms (100%), Space: 40.9 MB (58.81%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -1,16 +1,27 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int par=0,n=arr.length;
-        int sum=0,indsum=0;
-        for(int i=0;i<n;i++)
+        // int par=0,n=arr.length;
+        // int sum=0,indsum=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     indsum+=i;
+        //     sum+=arr[i];
+        //     if(indsum==sum)
+        //     {
+        //         par++;
+        //     }
+        // }
+
+        int chunks=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++)
         {
-            indsum+=i;
-            sum+=arr[i];
-            if(indsum==sum)
+            max=Math.max(max,arr[i]);
+            if(max==i)
             {
-                par++;
+                chunks++;
             }
         }
-        return par;
+        return chunks;
     }
 }
