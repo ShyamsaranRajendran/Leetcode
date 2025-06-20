0766939396650866f2f6c08daec0aa40ef3e157e
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-20 15:48:52 +0530

    Time: 2 ms (99.7%), Space: 44.6 MB (39.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,19 @@
 class Solution {
-    
-    int func(int i,int prev,int []nums,int[][]dp)
-    {
-        if(i==nums.length)
-        {
-            return 0;
-        }
-        if(dp[i][prev+1]!=-1)
-        {
-            return dp[i][prev+1];
-        }
-        int nottake=func(i+1,prev,nums,dp);
-        int take=0;
-        if(prev == -1 || nums[prev] < nums[i])
-        {
-            take=1+ func(i+1,i,nums,dp);
-        }
-        return dp[i][prev+1]= Math.max(take,nottake);
-
-    }
     public int lengthOfLIS(int[] nums) {
-        int dp[][]=new int[nums.length][nums.length+1];
-        for(int i=0;i<nums.length;i++)
-        {
-            Arrays.fill(dp[i],-1);
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
+            }
+            tails[i] = x;
+            if (i == size) ++size;
         }
-        int ans=func(0,-1,nums,dp);
-        return ans;
+        return size;
     }
 }
