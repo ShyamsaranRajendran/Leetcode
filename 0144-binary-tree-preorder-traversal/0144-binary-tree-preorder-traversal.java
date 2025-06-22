5f7a32780b3e8a123f4f7be6945c022c4ae128b7
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-06-22 09:48:04 +0530

    Time: 0 ms (100%), Space: 42 MB (16.07%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,22 +14,31 @@
  * }
  */
 class Solution {
+    void func(List<Integer> list, TreeNode root)
+    {
+        if(root==null)
+           return ;
+        list.add(root.val);
+        func(list,root.left);
+        func(list,root.right);
+    }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res= new ArrayList<>();
-        Stack<TreeNode> stk=new Stack<>();
-        TreeNode cur=root;
-        if(root==null)
-        {
-            return res;
-        }
-        stk.push(root);
-        while(!stk.isEmpty())
-        {
-            cur=stk.pop();
-            res.add(cur.val);
-            if(cur.right!=null) stk.push(cur.right);
-            if(cur.left!=null) stk.push(cur.left);
-        }
+        // Stack<TreeNode> stk=new Stack<>();
+        // TreeNode cur=root;
+        // if(root==null)
+        // {
+        //     return res;
+        // }
+        // stk.push(root);
+        // while(!stk.isEmpty())
+        // {
+        //     cur=stk.pop();
+        //     res.add(cur.val);
+        //     if(cur.right!=null) stk.push(cur.right);
+        //     if(cur.left!=null) stk.push(cur.left);
+        // }
+        func(res,root);
         return res;
     }
 }
