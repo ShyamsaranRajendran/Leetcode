33cd84c23fbc2d544219ce4f362747167dc008cc
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-07-06 19:45:51 +0530

    Time: 0 ms (100%), Space: 44.8 MB (27.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,21 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null)
-        {
+        if(head==null || head.next==null){
             return false;
         }
-        ListNode fast=head,slow=head;
 
-        while(fast!=null && fast.next!=null)
-        {
+        ListNode slow=head,fast=head.next;
+
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast)
-            {
+            if(slow==fast){
                 return true;
             }
         }
+
         return false;
     }
 }
