573c46f8dd6c19cad3553ea9c882c8021a91dc76
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-08-19 21:38:51 +0530

    Time: 0 ms (100%), Space: 40.9 MB (16.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,31 +1,29 @@
 class Solution {
-    int func(int i,int j,int dp[][])
-    {
-        int n=dp.length,m=dp[0].length;
-        if(i<0 ||j<0 || j>=m || i>=n)
-        {
+
+    int func(int i,int j,int n,int m,int dp[][]){
+        if(i<0 || j<0 || i>n||j>m){
             return 0;
         }
-        if(i==n-1 && j==m-1)
-        {
+
+        if(i==n-1 && j==m-1){
             return 1;
         }
-        if(dp[i][j]!=-1)
-        {
+
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        
-        int down= func(i+1,j,dp);
-        int right= func(i,j+1,dp);
 
-        return dp[i][j]= down+right;
+        int down = func(i,j+1,n,m,dp);
+        int right = func(i+1,j,n,m,dp);
 
+        return dp[i][j]=down+right;
     }
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++)
-           Arrays.fill(dp[i],-1);
-        int ans= func(0,0,dp);
-        return ans;
+       int dp[][]=new int[m+1][n+1];
+       for(int i[]:dp){
+        Arrays.fill(i,-1);
+       }
+       int res=func(0,0,m,n,dp);
+       return res;
     }
 }
