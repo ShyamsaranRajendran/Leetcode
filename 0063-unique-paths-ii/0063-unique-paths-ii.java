9ad21cce5a1bf24ff0d9fc6bf93a66f1e5284246
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-08-19 21:46:57 +0530

    Time: 0 ms (100%), Space: 41.7 MB (81.74%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,32 +1,34 @@
 class Solution {
-     int func(int i,int j,int grid[][],int dp[][])
+    int func(int i,int j,int grid[][],int dp[][])
     {
-        int n=dp.length,m=dp[0].length;
-        if(i<0 ||j<0 || j>=m || i>=n || grid[i][j]==1)
-        {
+        int n=grid.length;
+        int m=grid[0].length;
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==1){
             return 0;
         }
-        if(i==n-1 && j==m-1)
-        {
+
+        if(i==n-1 && j==m-1){
             return 1;
         }
-        if(dp[i][j]!=-1)
-        {
+
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        
-        int down= func(i+1,j,grid,dp);
-        int right= func(i,j+1,grid,dp);
 
-        return dp[i][j]= down+right;
+        int down= func(i,j+1,grid,dp);
+        int right = func(i+1,j,grid,dp);
 
+        return dp[i][j] = down + right;
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n=obstacleGrid.length,m=obstacleGrid[0].length;
-           int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++)
-           Arrays.fill(dp[i],-1);
-        int ans= func(0,0,obstacleGrid,dp);
-        return ans;
+        int n=obstacleGrid.length;
+        int m=obstacleGrid[0].length;
+        int dp[][]=new int[n+1][m+1];
+
+        for(int i[]:dp){
+            Arrays.fill(i,-1);
+        }
+        int res= func(0,0,obstacleGrid,dp);
+        return res;
     }
 }
