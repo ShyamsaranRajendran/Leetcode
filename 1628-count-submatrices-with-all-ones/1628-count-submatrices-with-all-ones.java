8d39ba8b42418cb318526f90005e75457ca09151
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-08-21 20:16:29 +0530

    Time: 142 ms (5.14%), Space: 46 MB (20.89%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -1,27 +1,34 @@
 class Solution {
     public int numSubmat(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        int[] heights = new int[n];
-        int res = 0;
+        int r = mat.length, c = mat[0].length;
+        int[] h = new int[c];
+        int ans = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    heights[j] = 0;
-                } else {
-                    heights[j] += 1;
-                }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                h[j] = (mat[i][j] == 0) ? 0 : h[j] + 1;
             }
-
-            for (int j = 0; j < n; j++) {
-                int minHeight = heights[j];
-                for (int k = j; k >= 0 && minHeight > 0; k--) {
-                    minHeight = Math.min(minHeight, heights[k]);
-                    res += minHeight;
-                }
-            }
+            ans += count(h);
         }
+        return ans;
+    }
 
+    private int count(int[] h) {
+        int n = h.length, res = 0;
+        int[] sum = new int[n];
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && h[st.peek()] >= h[i]) st.pop();
+            if (!st.isEmpty()) {
+                int p = st.peek();
+                sum[i] = sum[p] + h[i] * (i - p);
+            } else {
+                sum[i] = h[i] * (i + 1);
+            }
+            st.push(i);
+            res += sum[i];
+        }
         return res;
     }
 }
