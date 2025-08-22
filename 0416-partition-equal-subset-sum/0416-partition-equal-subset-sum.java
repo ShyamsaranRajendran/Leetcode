d961f872496e3531cdbca2554c250aea4d31c1ee
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-08-22 19:52:42 +0530

    Time: 106 ms (11.19%), Space: 56.8 MB (39.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,29 +1,26 @@
 class Solution {
-
-    boolean func(int i , int[]nums,int cur ,int sum,Boolean dp[][])
-    {
-        if(cur == sum)
-        {
-            return true;
-        }
-        if(cur > sum || i>=nums.length)
-        {
+    boolean func(int i,int nums[],int cur,int sum,Boolean dp[][]){
+        if(i>=nums.length || cur > sum){
             return false;
         }
-        if(dp[i][cur]!= null) return dp[i][cur];
-        boolean pick = func(i+1,nums,cur+nums[i],sum,dp);
-        boolean notpick= func(i+1,nums,cur,sum,dp);
+        if(cur==sum){
+            return true;
+        }
+        if(dp[i][cur]!=null){
+            return dp[i][cur];
+        }
+        Boolean pick = func(i+1,nums,cur+nums[i],sum,dp);
+        Boolean notpick = func(i+1,nums,cur,sum,dp);
 
-        return dp[i][cur] = pick||notpick;
+        return dp[i][cur]= pick || notpick;
     }
     public boolean canPartition(int[] nums) {
-       int sum=0;
-       for(int i :  nums) sum+=i;
-      
-       if(sum%2==1) return false;
-       sum=sum/2;
-       Boolean dp[][]=new Boolean[nums.length][sum];
-       boolean res=  func(0,nums,0,sum,dp);
-       return res;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        Boolean dp[][]=new Boolean[nums.length +1][target+1];
+        boolean res=func(0,nums,0,target,dp);
+        return res;
     }
 }
