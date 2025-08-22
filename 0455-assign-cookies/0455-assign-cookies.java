0b002ad140977f7906569a4dc12d0b581c07d771
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-08-22 21:15:24 +0530

    Time: 9 ms (98.8%), Space: 48.5 MB (59.6%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(s);
+        Arrays.sort(g);
+
+        int i = 0, j = 0, n = g.length, m = s.length;
+
+        while (i<n && j<m) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+
+        return i;
+    }
+}
