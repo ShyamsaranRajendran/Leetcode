0f5c503846c1906b85637afe54a0d7da531522a2
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-09-02 21:33:34 +0530

    Time: 0 ms (100%), Space: 41.1 MB (67.92%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -13,32 +13,38 @@
  *     }
  * }
  */
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    void func(TreeNode root,ArrayList<Integer> list,String temp){
-        if(root == null){
-           return;
-        }
+    int result = 0;
+    public int sumNumbers(TreeNode root) {
+        preOrder(root, 0);
+        return result;
+    }
 
-        temp=temp + root.val;
-        
-        if (root.left == null && root.right == null) {
-            list.add(Integer.parseInt(temp));
+    public void preOrder(TreeNode root, int curr) {
+        if (root != null) {
+            curr = curr * 10 + root.val;
+            if (root.left == null && root.right == null) {
+            result += curr;
             return;
         }
-
-        func(root.left, list, temp);
-        func(root.right, list, temp);
-
-    }
-    public int sumNumbers(TreeNode root) {
-        int res=0;
-
-        if(root == null) return 0;
-        ArrayList<Integer> list= new ArrayList<>();
-        func(root,list,new String());
-        for(int i:list){
-            res+=i;
+        preOrder(root.left, curr);
+        preOrder(root.right, curr);
         }
-        return res;
+        
     }
 }
