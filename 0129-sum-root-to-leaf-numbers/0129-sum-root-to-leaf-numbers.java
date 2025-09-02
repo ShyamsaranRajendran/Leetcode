a2d516a7a8ddb976cbed0868290ba0287c1f83b8
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-09-02 21:35:59 +0530

    Time: 0 ms (100%), Space: 41.1 MB (67.92%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -28,23 +28,18 @@
  *     }
  * }
  */
-class Solution {
-    int result = 0;
+
+ class Solution {
     public int sumNumbers(TreeNode root) {
-        preOrder(root, 0);
-        return result;
+        return dfs(root, 0);
     }
 
-    public void preOrder(TreeNode root, int curr) {
-        if (root != null) {
-            curr = curr * 10 + root.val;
-            if (root.left == null && root.right == null) {
-            result += curr;
-            return;
+    private int dfs(TreeNode node, int currentSum) {
+        if (node == null) return 0;
+        currentSum = currentSum * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            return currentSum;
         }
-        preOrder(root.left, curr);
-        preOrder(root.right, curr);
-        }
-        
+        return dfs(node.left, currentSum) + dfs(node.right, currentSum);
     }
 }
