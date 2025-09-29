dd463e36365f957c44ab18afe6a7ec17ccb6bef4
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-09-29 22:11:40 +0530

    Time: 2 ms (99.07%), Space: 41.4 MB (52.01%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -1,15 +1,30 @@
+
 class Solution {
+    int dp[][];
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
-        int[][] dp = new int[n][n];
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = i + 1; k < j; ++k) {
-                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],
-                        dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
-                }
-            }
+        dp = new int[n][n];
+        for (int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        // call the function with i = 1, j = n-1
+        // because every time we have to take the ar[i-1] that's why we are starting from i = 1
+        return helper_min_score(1,n-1,values);
+    }
+    
+    private int helper_min_score(int i,int j,int values[]){
+        if (i == j) return 0;
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        int mini = Integer.MAX_VALUE;
+        // do partition
+    
+        for (int k=i;k<=j-1;k++){
+            int steps = values[i-1] * values[k] * values[j]
+                + helper_min_score(i,k,values)// partition 1
+                + helper_min_score(k+1,j,values); // partition 2
+            mini = Math.min(steps,mini);
         }
-        return dp[0][n - 1];
+        
+        return dp[i][j] = mini;
     }
 }
