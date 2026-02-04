1400359bd041669da11728b0e6d6dd62ae2de445
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2026-02-04 22:31:32 +0530

    Time: 0 ms (100%), Space: 44.8 MB (50.63%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -5,27 +5,24 @@ class Solution {
 
         int i = 1;
 
-        // 1️⃣ Strictly increasing
         while (i < n && nums[i] > nums[i - 1]) {
             i++;
         }
         if (i == 1 || i == n) return false;
 
-        // 2️⃣ Strictly decreasing
+    
         int decStart = i;
         while (i < n && nums[i] < nums[i - 1]) {
             i++;
         }
         if (i == decStart || i == n) return false;
 
-        // 3️⃣ Strictly increasing again
         int inc2Start = i;
         while (i < n && nums[i] > nums[i - 1]) {
             i++;
         }
         if (i == inc2Start) return false;
 
-        // Must reach the end exactly
         return i == n;
     }
 }
