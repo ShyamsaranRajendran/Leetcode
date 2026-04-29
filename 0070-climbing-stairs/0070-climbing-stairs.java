b97f6ac04c4fb591146312ade312875dc9f78928
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2026-04-29 23:18:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,7 @@
 class Solution {
-    int func(int i,int dp[]){
-        if(i==0) return 1;
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=func(i-1,dp) + func(i-2,dp);
-    }
     public int climbStairs(int n) {
-        int ans=0;
-        int dp[]= new int[n+1];
-        Arrays.fill(dp,-1);
-        ans=func(n,dp);
-        return ans;
+        if(n == 0) return 1;
+        else if(n <0) return 0;
+        else return climbStairs(n-1) + climbStairs(n-2);
     }
 }
