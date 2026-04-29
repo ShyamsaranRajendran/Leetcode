3d3c565e31d718501d0ad817d10fc1185dc19a8b
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2026-04-29 23:19:30 +0530

    Time: 0 ms (100%), Space: 42 MB (72.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,20 @@
 class Solution {
+
+    int func(int i,int n,int dp[]){
+        if(i==n)
+           return 1;
+        if(i>n)
+           return 0;
+        if(dp[i]!=-1)
+           return dp[i];
+        int oneway=func(i+1,n,dp);
+        int secondway=func(i+2,n,dp);
+        return dp[i]=oneway+secondway;
+    }
     public int climbStairs(int n) {
-        if(n == 0) return 1;
-        else if(n <0) return 0;
-        else return climbStairs(n-1) + climbStairs(n-2);
+        int dp[]= new int[n];
+        Arrays.fill(dp,-1);
+        int res= func(0,n,dp);
+        return res;
     }
 }
