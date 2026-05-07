eda721b9e371c27237ff1de9e7595f457642aad3
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2026-05-07 23:13:45 +0530

    Time: 7 ms (92.6%), Space: 124.7 MB (76.85%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,28 +1,29 @@
 class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
-        int  n = boxGrid.length;
-        int m = boxGrid[0].length;
+        int n=boxGrid.length;
+        int m=boxGrid[0].length;
 
-        for(int i=0; i<n; i++){
-            int empty = m - 1;
-            for(int j=m-1; j>=0; j--){
-                if(boxGrid[i][j] =='*'){
-                    empty = j - 1;
+        for(int i=0;i<n;i++){
+            int empty = m-1;
+            for(int j=m-1;j>=0;j--){
+                if(boxGrid[i][j]=='*'){
+                    empty = j-1;
                 }else if(boxGrid[i][j] == '#'){
-                    boxGrid[i][j] = '.';
-                    boxGrid[i][empty] = '#';
+                    char t=boxGrid[i][j];
+                    boxGrid[i][j] = boxGrid[i][empty];
+                    boxGrid[i][empty] = t;
                     empty--;
                 }
             }
         }
 
-        char[][] result = new char[m][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                result[j][n-i-1] = boxGrid[i][j];
+        char res[][]= new char [m][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                res[j][n-i-1] = boxGrid[i][j];
             }
         }
 
-        return result;
+        return res;
     }
 }
